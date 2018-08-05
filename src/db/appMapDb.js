148d5a2c9f8e5f6578f648d202861e8c68ec54b4
iijx <iijx@qq.com>
2018-08-06 01:09:10 +0800

    8/5晚未完待续

---
+++ src/db/appMapDb.js
@@ -0,0 +1,4 @@
+export default {
+    // vae嵩迷们
+    wxf32a027f63a045c5: 'User_vae',
+}
---
+++ src/db/schema/user_vae.js
@@ -0,0 +1,31 @@
+
+let mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+    openid: {
+        unique: true,
+        required: true,
+        type: String,
+    },
+    nickname: {
+        type: String,
+    },
+    avatarUrl: {
+        type: String,
+    },
+    meta: {
+        created: {
+            type: Date,
+            default: Date.now(),
+        },
+        updated: {
+            type: Date,
+            default: Date.now(),
+        }
+    }
+})
+
+mongoose.model('User_vae', UserSchema);
