9fd6c97d7efbcae0d6b016f7d0d872e4751761c8
iijx <iijx@qq.com>
2018-10-03 14:37:08 +0800

    test

--- src/middleWares/router/index.js
+++ src/middleWares/router/index.js
@@ -7,5 +7,13 @@ const router = new Router();
 router.post('/nologin/login', c_user.login)
 router.post('/nologin/create', c_user.create)
 
-
+router.get(`/isCopy`, async (ctx, next) => {
+    ctx.body = {
+        success: true,
+        result: {
+            isCopy: true,
+            copyText: "DCWvhj67ZE"
+        }
+    };
+});
 module.exports = router;
